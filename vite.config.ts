92e26f9e8e1ba3274eae221b1d70fa67ad5395a9
genialtechie <only1hbo@icloud.com>
2023-11-29 16:40:17 -0500

    added fonts and assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Unfonts from 'unplugin-fonts/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    Unfonts({
+      custom: {
+        families: [
+          {
+            name: 'Larken',
+            local: ['Larken', 'Larken-Variable'],
+            src: './src/assets/fonts/*.ttf',
+          },
+        ],
+      },
+    }),
+  ],
+});
