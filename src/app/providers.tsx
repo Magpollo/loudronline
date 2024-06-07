464c4e9be6b5d0e9eb9c4c82f6d67574783a7abe
genialtechie <only1hbo@icloud.com>
2024-06-07 01:33:58 -0400

    added chakra theme file

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider as NextThemeProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes/dist/types';
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from '@/utils/theme';
 
 export default function Providers({ children, ...props }: ThemeProviderProps) {
   return (
@@ -13,7 +14,7 @@ export default function Providers({ children, ...props }: ThemeProviderProps) {
       attribute="class"
     >
       <CacheProvider>
-        <ChakraProvider>{children}</ChakraProvider>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
       </CacheProvider>
     </NextThemeProvider>
   );
